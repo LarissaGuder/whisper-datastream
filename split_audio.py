0bbc5ae767ca67b608756aba7a1d2cef34880fe5
larissaguder <lariguder10@gmail.com>
2022-10-21 02:08:47 -0300

    processing in memory

--- split_audio.py
+++ split_audio.py
@@ -3,73 +3,48 @@ from pydub.silence import split_on_silence
 import pydub
 import numpy as np 
 import time
-
-sound = AudioSegment.from_mp3("./Recording (3).mp3")
-chunks = split_on_silence(
-    sound,
-
-    # split on silences longer than 1000ms (1 sec)
-    min_silence_len=500,
-
-    # anything under -16 dBFS is considered silence
-    silence_thresh=-40, 
-
-)
-
-# now recombine the chunks so that the parts are at least 90 sec long
-target_length = 10 * 1000
-# print(chunks)
-output_chunks = [chunks[0]]
-chunk_to_np = []
-for chunk in chunks[1:]:
-    if len(output_chunks[-1]) < target_length:
-        output_chunks[-1] += chunk
-    else:
-        # if the last output chunk is longer than the target length,
-        # we can start a new one
-        output_chunks.append(chunk)
-
-        # 
-
-
-# OK isso aqui tem falhado miseravelmente na transcriçao.
-# I'm going to start with the sound of the sound.
-# I'm going to do a little bit of the same thing.
-# I'm sorry.
-# I'm going to do a little bit of the same thing.
-# I'm going to do a little bit of the same thing.
-# I'm going to use the same method as the other one.
-for chunk in output_chunks:
-    chunk_to_np.append(np.frombuffer(chunk.get_array_of_samples(), np.int16).flatten().astype(np.float32) / 32768.0)
-
-# output_chunks = np.frombuffer(output_chunks.get_array_of_samples(), np.int16).flatten().astype(np.float32) / 32768.0
-
-# for i, chunk in enumerate(output_chunks):
-#    output_file = "./audio/chunk{0}.mp3".format(i)
-#    print("Exporting file", output_file)
-#    chunk.export(output_file, format="mp3")
-
-start_time = time.time()
-
-# print(output_chunks)
 import whisper
 model = whisper.load_model("base")
 
-for i, chunk in enumerate(chunk_to_np):
-    print(chunk)
-    path = "./audio/chunk{0}.mp3".format(i)
-# load audio and pad/trim it to fit 30 seconds
-    # audio = whisper.load_audio(chunk)
-    audio = whisper.pad_or_trim(chunk)
+sound = AudioSegment.from_wav("./audio2.wav")
+# sound = AudioSegment.from_mp3("./Gusttavo Lima é o SUS do sertanejo _ Choque de Cultura - Ambiente de Música #CORTE.mp3")
 
+# Set to mono
+sound = sound.set_channels(1)
+
+# Split audio on silence.
+chunks = split_on_silence(
+    sound,
+    # split on silences longer than 1000ms (1 sec)
+    min_silence_len=500,
+    # anything under -16 dBFS is considered silence
+    silence_thresh=-40, 
+)
+
+start_time = time.time()
+
+target_length = 10 * 1000
+# print(chunks)
+# output_chunks = [chunks[0]]
+# for chunk in chunks[1:]:
+#     if len(output_chunks[-1]) < target_length:
+#         output_chunks[-1] += chunk
+#     else:
+#         # if the last output chunk is longer than the target length,
+#         # we can start a new one
+#         output_chunks.append(chunk)
+
+for chunk in chunks:
+    audio = np.frombuffer(chunk.get_array_of_samples(), np.int16).flatten().astype(np.float32) / 32768.0
+    audio = whisper.pad_or_trim(audio)
     # make log-Mel spectrogram and move to the same device as the model
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
     # detect the spoken language
-    _, probs = model.detect_language(mel)
+    # _, probs = model.detect_language(mel)
     # print(f"Detected language: {max(probs, key=probs.get)}")
 
     # decode the audio
+    #  to define the language >> language = 'portuguese'
     options = whisper.DecodingOptions(fp16 = False)
     result = whisper.decode(model, mel, options)
 
